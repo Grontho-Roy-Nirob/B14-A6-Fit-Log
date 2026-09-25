10bf389ef9480ac3ecf0593abe3e9fbf31a60a0e
Grontho-Roy-Nirob <gronthoroy2023@gmail.com>
2026-09-25 11:26:01 +0600

    Add workout API service functions

---
+++ fitness-hub/src/lib/api.ts
@@ -0,0 +1,46 @@
+import { IWorkout } from "@/types/workout.type";
+
+const BASE_URL = process.env.NEXT_PUBLIC_SERVER_BASE_URL;
+
+export const getWorkouts = async (): Promise<IWorkout[] | null> => {
+  try {
+    const response = await fetch(`${BASE_URL}/fitlog`, {
+      cache: "no-store",
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch workouts");
+    }
+
+    const data = await response.json();
+    return data;
+
+  } catch(error) {
+      console.error("Error fetching workouts:", error);
+      return[];
+  }
+};
+
+
+export const getWorkout = async (
+  id: string,
+): Promise<IWorkout | null> => {
+  try {
+    const response = await fetch(`${BASE_URL}/fitlog/${id}`, {
+      cache: "no-store",
+    });
+
+    if (!response.ok) {
+      return null;
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("Error fetching workout:", error);
+
+    return null;
+  }
+};
+
