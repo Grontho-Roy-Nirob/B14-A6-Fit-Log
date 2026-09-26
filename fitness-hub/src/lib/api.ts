b79498bb37860fad034e0e840491a423e05483a9
Grontho-Roy-Nirob <gronthoroy2023@gmail.com>
2026-09-26 12:38:21 +0600

    cache no store removed

--- fitness-hub/src/lib/api.ts
+++ fitness-hub/src/lib/api.ts
@@ -4,9 +4,7 @@ const BASE_URL = process.env.NEXT_PUBLIC_SERVER_BASE_URL;
 
 export const getWorkouts = async (): Promise<IWorkout[]> => {
   try {
-    const response = await fetch(`${BASE_URL}/fitlog`, {
-      cache: "no-store",
-    });
+    const response = await fetch(`${BASE_URL}/fitlog`);
 
     if (!response.ok) {
       throw new Error("Failed to fetch workouts");
